bb379a83c9409b86a9de9000a51c16d17b6cf311
Daniel <dkraem2s@smail.inf.h-brs.de>
2013-04-11 22:53:39 +0200

    Initialer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookWriter::Application.config.secret_token = '6239e115cf47c9727c3c43143ea181a854c8a0a867a7e3cd5bf790c8e01879f9161aa850ff59f867fa20ba23da88ff5af6ab1937cb27d59d7a8979b4460bac99'
